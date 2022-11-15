c123c3399e4634ea1fdaf956e734681cc62397ac
Pablo <84303588+Puetate@users.noreply.github.com>
2022-11-14 23:13:18 -0700

    socket succes

--- Socket/Socket.ino
+++ Socket/Socket.ino
@@ -9,14 +9,13 @@ SocketIoClient webSocket;
 const char* ssid = "PuetaWifi";
 const char* password = "pueta@localhost:8080";
 const char* HOST = "137.184.70.22";
-
 //evento webSocket
 void event(const char * payload, size_t length){
   Serial.printf("Activando alarma de emergencia: %s\n", payload);
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   Serial.setDebugOutput(true);
   //Connectar a Wifi
@@ -33,7 +32,7 @@ void setup() {
 
   //Recibir evento desde el Seridor
   webSocket.on("alert",event);
-  webSocket.begin(HOST,3000);
+  webSocket.begin(HOST,3000,"/");
 
 }
 
@@ -46,6 +45,15 @@ void loop() {
     Serial.println("Conexion perdida");
   } */
   webSocket.loop();
-  delay(500);
+  
+  int count = 0;
+  count++;
+    if (count == 18000){
+        count = 0;
+
+        // Send data to Server
+        webSocket.emit("alert","Oee desde esp32");
+
+    }
 }
 
